9803303dbe8af2770aba13110f29267915305595
Anton K <mi3ta@sent.as>
2020-02-18 17:03:43 +0700

    :sparkles: Added 'AppFooter' component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <div id="app">
     <router-view/>
+    <app-footer />
   </div>
 </template>
+
+<script>
+import AppFooter from '@/components/AppFooter'
+export default {
+  components: {
+    AppFooter
+  }
+}
+</script>
 
 <style lang="scss">
 html, body {
   margin: 0;
   padding: 0;
+  height: 100%;
 
   * {
     box-sizing: border-box;
@@ -15,7 +26,10 @@ html, body {
 }
 
 #app {
-  font-family: sans-serif;
+  min-height: 100%;
+  display: grid;
+  grid-template-rows: 1fr auto;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
