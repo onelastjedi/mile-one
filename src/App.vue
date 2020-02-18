e39b091fbed5a7e1bf2f190147e15fb7d2169ad9
Anton K <mi3ta@sent.as>
2020-02-18 19:25:42 +0700

    :lipstick: Improved global styling

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@ html, body {
   margin: 0;
   padding: 0;
   height: 100%;
+  background-color: #191919;
 
   * {
     box-sizing: border-box;
@@ -29,8 +30,37 @@ html, body {
   min-height: 100%;
   display: grid;
   grid-template-rows: 1fr auto;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir Next', Helvetica, Arial, sans-serif;
+  font-size: 16px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
+  h1 {
+    font-family: 'Kulturista', sans-serif;
+    font-weight: normal;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  .small {
+    font-size: 10px;
+    line-height: 1.5;
+  }
+}
+
+@font-face {
+    font-family: 'Kulturista';
+    src: url('./assets/fonts/Kulturista-Regular.woff') format('woff');
+    font-style: normal;
+    font-weight: 400;
+}
+
+@font-face {
+  font-family: 'Avenir Next';
+  src: url('./assets/fonts/AvenirNext-Regular.woff') format('woff');
+  font-weight: 400;
+  font-style: normal;
 }
 </style>
