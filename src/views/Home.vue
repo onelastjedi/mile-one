c4165ca3b37e752d39b3470ac784ddcd10e4d669
Anton K <mi3ta@sent.as>
2020-02-21 01:22:12 +0700

    :sparkles: Injected new components into 'Home' view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="home">
     <app-header-mobile />
+    <section-features-mobile />
+    <section-big-photos-mobile />
+    <section-join-waitlist-mobile />
   </div>
 </template>
 
 <script>
 import AppHeaderMobile from '@/components/AppHeaderMobile'
+import SectionFeaturesMobile from '@/components/SectionFeaturesMobile'
+import SectionBigPhotosMobile from '@/components/SectionBigPhotosMobile'
+import SectionJoinWaitlistMobile from '@/components/SectionJoinWaitlistMobile'
 export default {
-  name: 'Home',
   components: {
-    AppHeaderMobile
+    AppHeaderMobile,
+    SectionFeaturesMobile,
+    SectionBigPhotosMobile,
+    SectionJoinWaitlistMobile
   }
 }
 </script>
