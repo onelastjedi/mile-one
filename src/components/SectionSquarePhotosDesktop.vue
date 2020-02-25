729594c51a4ec3f7e279e037da66b4c080f530ef
Anton K <mi3ta@sent.as>
2020-02-25 20:23:12 +0700

    :sparkles: Added 'SectionSquarePhotosDesktop' component

---
+++ src/components/SectionSquarePhotosDesktop.vue
@@ -0,0 +1,19 @@
+<template functional>
+  <div class="section-square-photos-desktop">
+    <img src="@/assets/images/desktop/square-01.jpg" />
+    <img src="@/assets/images/desktop/square-02.jpg" />
+    <img src="@/assets/images/desktop/square-03.jpg" />
+    <img src="@/assets/images/desktop/square-04.jpg" />
+  </div>
+</template>
+
+<style lang="scss">
+  .section-square-photos-desktop {
+    display: flex;
+
+    img {
+      display: inline-block;
+      width: 25%;
+    }
+  }
+</style>
