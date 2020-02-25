89e46182501c4baf9d6a40abd77b4f6eeeac7a39
Anton K <mi3ta@sent.as>
2020-02-26 02:17:40 +0700

    :lipstick: Fixed payment form on mobile

--- src/views/Pay.vue
+++ src/views/Pay.vue
@@ -80,9 +80,7 @@
         </div>
         <div class="disclaimer">
           <p><b>DISCLAIMER</b>: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
           <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
           <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
       </div>
@@ -267,7 +265,9 @@
       }
 
       .card {
-        width: 300px;
+        width: 50%;
+        min-width: 200px;
+        max-width: 300px;
       }
 
       input {
