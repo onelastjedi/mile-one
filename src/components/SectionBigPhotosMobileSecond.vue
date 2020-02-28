d1ffc8d20b6139caf9ceda4d42c502ac41bfa2b6
Anton K <mi3ta@sent.as>
2020-02-29 00:01:01 +0700

    :art: Optimized photos for 'SectionBigPhotosMobileSecond' component

--- src/components/SectionBigPhotosMobileSecond.vue
+++ src/components/SectionBigPhotosMobileSecond.vue
@@ -1,21 +1,39 @@
 <template functional>
   <div class="section-big-photos-mobile">
     <div class="image" key="00">
-      <img src="@/assets/images/IMG_6784.jpg" />
+      <img
+        srcset="
+          @/assets/images/mobile/one-bag-travel.jpg 1x,
+          @/assets/images/mobile/one-bag-travel@2x.jpg 2x
+        "
+        alt="Designed for One Bag Travel"
+      />
       <div class="caption">
         <h3>Designed for<br>One Bag Travel</h3>
         <p>A minimalist travel bag made from ultra strong<br>Dyneema® fabric.</p>
       </div>
     </div>
     <div class="image" key="02">
-      <img src="@/assets/images/20200119-DSC_5386.jpg" />
+      <img
+        srcset="
+          @/assets/images/mobile/rugged-and-handsome.jpg 1x,
+          @/assets/images/mobile/rugged-and-handsome@2x.jpg 2x
+        "
+        alt="Rugged and Handsome"
+      />
       <div class="caption">
         <h3>Rugged and Handsome</h3>
         <p>Built to withstand the rigors of global travel — <br>and look good doing it.</p>
       </div>
     </div>
     <div class="image" key="03">
-      <img src="@/assets/images/20200119-DSC_5258.jpg" />
+      <img
+        srcset="
+          @/assets/images/mobile/accessible-and-expansive.jpg 1x,
+          @/assets/images/mobile/accessible-and-expansive@2x.jpg 2x
+        "
+        alt="Accessible & Expansive"
+      />
       <div class="caption">
         <h3>Accessible & Expansive</h3>
         <p>The rolltop makes it easy to grab your gear and<br>expand the Mile One to hold more.</p>
