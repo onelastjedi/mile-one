af9f84eb444e63e3be0e705bb2db1391b9846486
Anton K <mi3ta@sent.as>
2020-02-28 23:50:23 +0700

    :sparkles: Optimized images for 'SectionFeaturesMobile' component

--- src/components/SectionFeaturesMobile.vue
+++ src/components/SectionFeaturesMobile.vue
@@ -2,37 +2,79 @@
   <div class="section-features-mobile">
     <h2>3 Ways to Carry:</h2>
     <figure>
-      <img src="@/assets/images/DSC00711.png" />
+      <img
+        srcset="
+          @/assets/images/mobile/feature-backpack.jpg 1x,
+          @/assets/images/mobile/feature-backpack@2x.jpg 2x
+        "
+        alt="Backpack"
+      />
       <figcaption>Backpack</figcaption>
     </figure>
     <div class="row">
       <figure>
-        <img src="@/assets/images/DSC00712.png" />
+        <img
+          srcset="
+            @/assets/images/mobile/feature-shoulder-pack.jpg 1x,
+            @/assets/images/mobile/feature-shoulder-pack@2x.jpg 2x
+          "
+          alt="Shoulder Pack"
+        />
         <figcaption>Shoulder Pack</figcaption>
       </figure>
       <figure>
-        <img src="@/assets/images/DSC00713.png" />
+        <img
+          srcset="
+            @/assets/images/mobile/feature-duffel-bag.jpg 1x,
+            @/assets/images/mobile/feature-duffel-bag@2x.jpg 2x
+          "
+          alt="Duffel Bag"
+        />
         <figcaption>Duffel Bag</figcaption>
       </figure>
     </div>
     <h2>Features:</h2>
     <div class="row">
       <figure>
-        <img src="@/assets/images/DSC00714.png" />
+        <img
+          srcset="
+            @/assets/images/mobile/feature-minimal-design.jpg 1x,
+            @/assets/images/mobile/feature-minimal-design@2x.jpg 2x
+          "
+          alt="Minimal Design"
+        />
         <figcaption>Minimal Design</figcaption>
       </figure>
       <figure>
-        <img src="@/assets/images/DSC00715.png" />
+        <img
+          srcset="
+            @/assets/images/mobile/feature-waterproof-zippers.jpg 1x,
+            @/assets/images/mobile/feature-waterproof-zippers@2x.jpg 2x
+          "
+          alt="Waterproof Zippers"
+        />
         <figcaption>Waterproof Zippers</figcaption>
       </figure>
     </div>
     <div class="row">
       <figure>
-        <img src="@/assets/images/DSC00716.png" />
+        <img
+          srcset="
+            @/assets/images/mobile/feature-petzl-carabiners.jpg 1x,
+            @/assets/images/mobile/feature-petzl-carabiners@2x.jpg 2x
+          "
+          alt="Petzl Carabiners"
+        />
         <figcaption>Petzl Carabiners</figcaption>
       </figure>
       <figure>
-        <img src="@/assets/images/DSC00717.png" />
+        <img
+          srcset="
+            @/assets/images/mobile/feature-handles-wear-gracefully.jpg 1x,
+            @/assets/images/mobile/feature-handles-wear-gracefully@2x.jpg 2x
+          "
+          alt="Handles wear gracefully"
+        />
         <figcaption>Handles wear gracefully</figcaption>
       </figure>
     </div>
