2b129fddd05b35a479e8c3e4416e43db850a0391
Anton K <mi3ta@sent.as>
2020-02-29 01:14:13 +0700

    :sparkles: Added optimized photos for 'SectionSquarePhotosMobile' component

--- src/components/SectionSquarePhotosDesktop.vue
+++ src/components/SectionSquarePhotosDesktop.vue
@@ -1,19 +1,18 @@
 <template functional>
   <div class="section-square-photos-desktop">
-    <img src="@/assets/images/square-01.jpg" />
-    <img src="@/assets/images/square-02.jpg" />
-    <img src="@/assets/images/square-03.jpg" />
-    <img src="@/assets/images/square-04.jpg" />
+    <img key="00" srcset="
+      @/assets/images/desktop/pictures-4-main-footer.jpg 1x,
+      @/assets/images/desktop/pictures-4-main-footer@2x.jpg 2x"
+      alt="Lifestyle photos"
+    />
   </div>
 </template>
 
 <style lang="scss">
   .section-square-photos-desktop {
-    display: flex;
-
     img {
-      display: inline-block;
-      width: 25%;
+      display: block;
+      width: 100%;
     }
   }
 </style>
