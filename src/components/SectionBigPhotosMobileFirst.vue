f329a147e860b5a1f8fab83e0dae0e3decd2dc8c
Anton K <mi3ta@sent.as>
2020-02-28 23:22:15 +0700

    :sparkles: Optimized images for 'SectionBigPhotosMobile'

--- src/components/SectionBigPhotosMobileFirst.vue
+++ src/components/SectionBigPhotosMobileFirst.vue
@@ -2,21 +2,30 @@
   <div class="section-big-photos-mobile">
     <div class="image" key="01">
       <img class="dyneema-logo" src="@/assets/images/dyneema-logo-transparent-white-2x.png" />
-      <img src="@/assets/images/DSC_5347.jpg" />
+      <img srcset="
+        @/assets/images/mobile/dyneema-fabric-feature.jpg 1x,
+        @/assets/images/mobile/dyneema-fabric-feature@2x.jpg 2x"
+        alt="Dyneema® Fabric" />
       <div class="caption">
         <h3>Dyneema® Fabric</h3>
         <p>Made from a revolutionary lightweight fabric that<br>has a tensile strength 15x’s stonger than steel.</p>
       </div>
     </div>
     <div class="image" key="02">
-      <img src="@/assets/images/material-plus-science.jpg" />
+      <img srcset="
+        @/assets/images/mobile/material-plus-science.jpg 1x,
+        @/assets/images/mobile/material-plus-science@2x.jpg 2x"
+        alt="Material + Science" />
       <div class="caption">
         <h3>Material <span>+</span> Science</h3>
         <p>Dyneema® fabric is made of ultra-high-molecular-weight polyethylene fibers sandwiched between two layers of film, resulting in a flexible and extremely durable material.</p>
       </div>
     </div>
     <div class="image" key="03">
-      <img src="@/assets/images/extreme-durability.jpg" />
+      <img srcset="
+        @/assets/images/mobile/extreme-durability.jpg 1x,
+        @/assets/images/mobile/extreme-durability@2x.jpg 2x"
+        alt="Extreme Durability" />
       <div class="caption">
         <h3>Extreme Durability</h3>
         <p>Used in environments where there is no compromise on performance, Dyneema® fabric is ultra lightweight, weatherproof and rip-proof.</p>
