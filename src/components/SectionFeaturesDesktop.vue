84ff328855bc11f25dc8e4d80a8fb5aa3b9e4568
Anton K <mi3ta@sent.as>
2020-02-29 00:48:45 +0700

    :sparkles: Added optimized images for 'SectionFeaturesDesktop'

--- src/components/SectionFeaturesDesktop.vue
+++ src/components/SectionFeaturesDesktop.vue
@@ -3,34 +3,76 @@
     <div :class="$style.row">
       <h2>3 Ways to Carry:</h2>
       <figure>
-        <img src="@/assets/images/feature-backpack.png" />
+        <img
+          srcset="
+            @/assets/images/desktop/feature-backpack.png 1x,
+            @/assets/images/desktop/feature-backpack@2x.png 2x
+          "
+          alt="Backpack"
+        />
         <figcaption>Backpack</figcaption>
       </figure>
       <figure>
-        <img src="@/assets/images/feature-duffel-bag.png" />
+        <img
+          srcset="
+            @/assets/images/desktop/feature-duffel-bag.png 1x,
+            @/assets/images/desktop/feature-duffel-bag@2x.png 2x
+          "
+          alt="Duffel Bag"
+        />
         <figcaption>Duffel Bag</figcaption>
       </figure>
       <figure>
-        <img src="@/assets/images/feature-shoulder-bag.png" />
+        <img
+          srcset="
+            @/assets/images/desktop/feature-shoulder-bag.png 1x,
+            @/assets/images/desktop/feature-shoulder-bag@2x.png 2x
+          "
+          alt="Shoulder Bag"
+        />
         <figcaption>Shoulder Bag</figcaption>
       </figure>
     </div>
     <div :class="$style.row">
       <h2>Features:</h2>
       <figure>
-        <img src="@/assets/images/feature-minimal-design.png" />
+        <img
+          srcset="
+            @/assets/images/desktop/feature-minimal-design.png 1x,
+            @/assets/images/desktop/feature-minimal-design@2x.png 2x
+          "
+          alt="Minimal design"
+        />
         <figcaption>Minimal design</figcaption>
       </figure>
       <figure>
-        <img src="@/assets/images/feature-waterproof-zippers.png" />
+        <img
+          srcset="
+            @/assets/images/desktop/feature-waterproof-zippers.png 1x,
+            @/assets/images/desktop/feature-waterproof-zippers@2x.png 2x
+          "
+          alt="Waterproof zippers"
+        />
         <figcaption>Waterproof zippers</figcaption>
       </figure>
       <figure>
-        <img src="@/assets/images/feature-petzl-carabiners.png" />
+        <img
+          srcset="
+            @/assets/images/desktop/feature-petzl-carabiners.png 1x,
+            @/assets/images/desktop/feature-petzl-carabiners@2x.png 2x
+          "
+          alt="Petzl carabiners"
+        />
         <figcaption>Petzl carabiners</figcaption>
       </figure>
       <figure>
-        <img src="@/assets/images/feature-handles-wear-gracefully.png" />
+        <img
+          srcset="
+            @/assets/images/desktop/feature-handles-wear-gracefully.png 1x,
+            @/assets/images/desktop/feature-handles-wear-gracefully@2x.png 2x
+          "
+          alt="Handles wear gracefully"
+        />
         <figcaption>Handles wear gracefully</figcaption>
       </figure>
     </div>
