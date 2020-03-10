ffdd97341a80c966e1d9a3c5a0995709624f63ad
Anton K <mi3ta@sent.as>
2020-03-10 19:25:53 +0700

    :lipstick: Styled text for 'PrefaceMobile'

--- src/components/PrefaceMobile.vue
+++ src/components/PrefaceMobile.vue
@@ -1,15 +1,17 @@
 <template functional>
   <div class="preface-mobile">
-    The Mile One is in the final stages of development and you can help us launch by reserving yours today for only $1. The Mile One is in the final stages of development and you can help us launch by reserving yours today for only $1. The Mile One is in the final stages of development and you can help us launch by reserving yours today for only $1. The Mile One is in the final stages of development and you can help us launch by reserving yours today for only $1.
+    <p :key="0">At Black Mile, we’re passionate about travel and packing light. Over the past two years we’ve designed and tested countless iterations to create what we believe is the <b>ultimate travel bag.</b></p>
+    <p :key="1">We need your help to launch on Kickstarter and bring this project to life. When you sign up, you’ll also get access to our <b>early-bird discounts</b> on the Mile One. We’re looking forward to starting this journey with you.</p>
+    <p :key="2">— Charley, Founder of Black Mile</p>
   </div>
 </template>
 
 <style lang="scss">
   .preface-mobile {
-    background-color: #F9F9F9;
+    background-color: #E9E1DE;
     color: #000000;
     font-size: 16px;
-    padding: 20px;
+    padding: 25px 20px;
     text-align: center;
     line-height: 1.5;
   }
