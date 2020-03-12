a51b3ac0c5af7e21023e8d653739b3351b685603
Anton K <mi3ta@sent.as>
2020-03-12 15:23:44 +0700

    :sparkles: Added 'subscribe' api endpoint

---
+++ src/api/subscribe.js
@@ -0,0 +1,17 @@
+import fetch from 'node-fetch'
+const { MAILCHIMP_API_URL, MAILCHIMP_API_KEY } = process.env
+
+export default (req, res) => {
+  const body = {
+    email_address: req.body.email,
+    status: 'subscribed'
+  }
+
+  fetch(MAILCHIMP_API_URL, {
+    method: 'post',
+    body: JSON.stringify(body),
+    headers: { 'Authorization': `Basic ${MAILCHIMP_API_KEY}` },
+  })
+  .then(response => response.json())
+  .then(json => res.send(json));
+}
