a3ab876cc2361336de1cf327a94c228c7490704c
Anton K <mi3ta@sent.as>
2020-07-28 06:34:25 +0700

    Added "Product" view

---
+++ src/views/Product.vue
@@ -0,0 +1,391 @@
+<template>
+  <section>
+    <div class="product mobile">
+      <div class="product--header">
+        <svg width="283" height="21" viewBox="0 0 283 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 2.22718V0.222709H10.0965C13.5115 0.222709 15.5902 1.93021 15.5902 5.34521C15.5902 7.57239 14.6994 9.05717 13.1403 9.79957C14.9963 10.4677 16.0357 12.101 16.0357 14.5509C16.0357 17.9659 14.0312 19.7476 10.542 19.7476H0.0742438V17.7432H2.74685V2.22718H0ZM5.04827 8.98295H9.79958C12.1752 8.98295 13.2888 7.79513 13.2888 5.64219C13.2888 3.48925 12.101 2.30142 9.79958 2.30142H5.04827V8.98295ZM10.1708 17.7432C12.4722 17.7432 13.66 16.5553 13.66 14.4024C13.66 12.1752 12.5464 10.9874 10.1708 10.9874H4.97403V17.7432H10.1708Z" fill="white"/>
+          <path d="M41.574 2.22718H38.2332V17.7432H46.1768V13.5858H48.4782V19.7476H33.2592V17.7432H35.9318V2.22718H33.2592V0.222709H41.574V2.22718Z" fill="white"/>
+          <path d="M64.2912 17.7432H66.7411L73.4226 0.222709H76.2437L82.9252 17.7432H85.2267V19.7476H77.9512V17.7432H80.4754L78.9163 13.7343H70.6015L69.0425 17.7432H71.7151V19.7476H64.3654V17.7432H64.2912ZM71.1955 11.7298H78.2482L74.7589 2.52413L71.1955 11.7298Z" fill="white"/>
+          <path d="M114.403 6.38458C114.403 3.56349 112.472 1.93022 109.429 1.93022C106.162 1.93022 104.232 3.71195 104.232 6.90424V12.9919C104.232 16.1842 106.162 17.9659 109.429 17.9659C112.547 17.9659 114.477 16.3326 114.477 13.5115V13.4373H116.778V13.5115C116.778 17.6689 113.957 19.9704 109.429 19.9704C104.826 19.9704 101.93 17.4462 101.93 12.9919V6.97848C101.93 2.59837 104.677 0 109.206 0C111.656 0 113.586 0.668149 114.7 2.15293L114.997 0.222709H116.778V6.53304H114.477V6.38458H114.403Z" fill="white"/>
+          <path d="M145.286 0.222709H153.156V2.22718H150.854L143.95 8.68598L151.225 17.7432H153.601V19.7476H145.732V17.7432H148.182L142.168 10.1708L139.273 12.9176V17.7432H141.945V19.7476H134.299V17.7432H136.971V2.22718H134.299V0.222709H141.945V2.22718H139.273V10.3193L147.81 2.22718H145.286V0.222709Z" fill="white"/>
+          <path d="M175.799 17.7432H178.471V19.7476H170.824V17.7432H173.497V2.22718H170.824V0.222709H175.724L181.886 11.804L188.048 0.222709H192.948V2.22718H190.275V17.7432H192.948V19.7476H185.375V17.7432H188.048V4.75131L181.96 16.1841L175.799 4.75131V17.7432Z" fill="white"/>
+          <path d="M243.876 2.22718H240.535V17.7432H248.479V13.5858H250.78V19.7476H235.487V17.7432H238.16V2.22718H235.487V0.222709H243.802V2.22718H243.876Z" fill="white"/>
+          <path d="M270.379 2.22718H267.707V0.222709H283V5.79065H280.699V2.22718H272.755V8.98295H279.956V10.9874H272.755V17.7432H280.699V14.1797H283V19.7476H267.707V17.7432H270.379V2.22718Z" fill="white"/>
+          <path d="M218.189 20.2673H210.023V18.0401H212.992V2.44989H210.023V0.222709H218.189V2.44989H215.22V18.0401H218.189V20.2673Z" fill="white"/>
+        </svg>
+        <div class="product--header--burger"></div>
+      </div>
+      <div class="product--content">
+        <div class="product--container">
+          <h4>The Mile One <span>$499.00</span></h4>
+        </div>
+
+        <img src="@/assets/images/mobile/product/01.png" class="product--fw-img" />
+
+        <div class="product--container">
+          <button class="product--btn">Add to cart</button>
+          <p>
+            The Mile One is designed to be the world’s best one bag travel bag. Through extensive research and development, choosing only the highest quality materials and components, it was refined over countless iterations to be your “go to” bag.
+          </p>
+          <p>
+            We admit not everyone has the same attention to detail and eye for perfection. The Mile One is made for discerning individuals who appreciate intelligent design and expert craftsmanship — without compromises.
+          </p>
+        </div>
+
+        <img src="@/assets/images/mobile/product/02.png" class="product--fw-img" />
+        <img src="@/assets/images/mobile/product/03.png" class="product--fw-img" />
+        <img src="@/assets/images/mobile/product/04.png" class="product--fw-img" />
+
+        <div class="product--container">
+          <h5>Materials</h5>
+          <ul>
+            <li>Fabric: Dyneema®, the world’s strongest fiber™</li>
+            <li>Hardware: Aluminum Carabiners and Closures</li>
+          </ul>
+
+          <h5>Development</h5>
+          <p>Typical travel bags are big, clunky, noisome and troublesome. They feel like “luggage” when you are “lugging” them around. Many bags have useful features, yet they all seem to be lacking one. Or, on the flip side, many have far too many pockets or closures to make them practical for everyday use.</p>
+
+          <h5>Rationale</h5>
+          <p>The Mile One is the first travel bag designed by Black Mile founder Charley with his friend and admitted gear geek, Dan. Based on our experiences traveling the globe and testing gear from dozens of manufacturers, we worked through countless prototypes to create the Mile One.</p>
+          <p>We kept everything we liked and got rid of everything we didn’t like — or need — in the process. The result is a minimalist design that maximizes versatility through a multi-functional approach.</p>
+
+          <button class="product--btn">Add to cart</button>
+        </div>
+
+        <!-- <img src="@/assets/images/mobile/product/05.png" class="product--fw-img" /> -->
+        <video width="100%" controls preload="true" poster="@/assets/images/mobile/product/05.png">
+          <source src="@/assets/video/blackmile_backpack.mp4" type="video/mp4">
+          Sorry, your browser doesn't support embedded videos.
+        </video>
+
+        <img src="@/assets/images/mobile/product/06.png" class="product--fw-img" />
+        <img src="@/assets/images/mobile/product/07.png" class="product--fw-img" />
+        <img src="@/assets/images/mobile/product/08.png" class="product--fw-img" />
+        <img src="@/assets/images/mobile/product/09.png" class="product--fw-img" />
+        <img src="@/assets/images/mobile/product/10.png" class="product--fw-img" />
+
+        <!-- <img src="@/assets/images/mobile/product/11.png" class="product--fw-img" /> -->
+        <video width="100%" controls preload="true" poster="@/assets/images/mobile/product/11.png">
+          <source src="@/assets/video/blackmile_shoulder.mp4" type="video/mp4">
+          Sorry, your browser doesn't support embedded videos.
+        </video>
+
+        <img src="@/assets/images/mobile/product/12.png" class="product--fw-img" />
+
+        <div class="product--container">
+          <button class="product--btn">Add to cart</button>
+        </div>
+
+        <img src="@/assets/images/mobile/product/13.png" class="product--fw-img" />
+        <img src="@/assets/images/mobile/product/14.png" class="product--fw-img" />
+        <img src="@/assets/images/mobile/product/15.png" class="product--fw-img" />
+        <img src="@/assets/images/mobile/product/16.png" class="product--fw-img" />
+
+        <!-- <img src="@/assets/images/mobile/product/17.png" class="product--fw-img" /> -->
+        <video width="100%" controls preload="true" poster="@/assets/images/mobile/product/17.png">
+          <source src="@/assets/video/blackmile_closing.mp4" type="video/mp4">
+          Sorry, your browser doesn't support embedded videos.
+        </video>
+
+        <img src="@/assets/images/mobile/product/18.png" class="product--fw-img" />
+        <img src="@/assets/images/mobile/product/19.png" class="product--fw-img" />
+        <img src="@/assets/images/mobile/product/20.png" class="product--fw-img" />
+        <img src="@/assets/images/mobile/product/21.png" class="product--fw-img" />
+        <img src="@/assets/images/mobile/product/22.png" class="product--fw-img" />
+
+        <!-- <img src="@/assets/images/mobile/product/23.png" class="product--fw-img" /> -->
+        <video width="100%" controls preload="true" poster="@/assets/images/mobile/product/23.png">
+          <source src="@/assets/video/blackmile_unpack.mp4" type="video/mp4">
+          Sorry, your browser doesn't support embedded videos.
+        </video>
+
+        <div class="product--container">
+          <button class="product--btn">Add to cart</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="product desktop">
+      <div class="product--content">
+        <div class="product--container">
+          <div class="product--grid">
+            <div class="product--column">
+              <img src="@/assets/images/mobile/product/01.png" class="product--fw-img" />
+              <img src="@/assets/images/mobile/product/02.png" class="product--fw-img" />
+              <img src="@/assets/images/mobile/product/03.png" class="product--fw-img" />
+              <img src="@/assets/images/mobile/product/04.png" class="product--fw-img" />
+            </div>
+
+            <div class="product--column">
+              <h4>The Mile One <span>$499.00</span></h4>
+              <p>The Mile One is designed to be the world’s best one bag travel bag. Through extensive research and development, choosing only the highest quality materials and components, it was refined over countless iterations to be your “go to” bag.</p>
+              <p>We admit not everyone has the same attention to detail and eye for perfection. The Mile One is made for discerning individuals who appreciate intelligent design and expert craftsmanship — without compromises.</p>
+              <button class="product--btn">Add to cart</button>
+
+              <h5>Materials</h5>
+              <ul>
+                <li>Fabric: Dyneema®, the world’s strongest fiber™</li>
+                <li>Hardware: Aluminum Carabiners and Closures</li>
+              </ul>
+
+              <h5>Rationale</h5>
+              <p>Typical travel bags are big, clunky, noisome and troublesome. They feel like “luggage” when you are “lugging” them around. Many bags have useful features, yet they all seem to be lacking one. Or, on the flip side, many have far too many pockets or closures to make them practical for everyday use.</p>
+
+              <h5>Development</h5>
+              <p>The Mile One is the first travel bag designed by Black Mile founder Charley with his friend and admitted gear geek, Dan. Based on our experiences traveling the globe and testing gear from dozens of manufacturers, we worked through countless prototypes to create the Mile One.</p>
+              <p>We kept everything we liked and got rid of everything we didn’t like — or need — in the process. The result is a minimalist design that maximizes versatility through a multi-functional approach.</p>
+
+              <table>
+                <thead>
+                  <tr>
+                    <td><h5>Feature</h5></td>
+                    <td><h5>Benefit</h5></td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td style="padding-right: 20px; width: 40%"><b>Backpack, shoulder pack or duffle</b></td>
+                    <td>Multi-function design allows you to configure the pack based on use and load.</td>
+                  </tr>
+                  <tr>
+                    <td style="padding-right: 20px; width: 40%"><b>Opens like a suitcase</b></td>
+                    <td>Easily access the contents without “digging” to get to the bottom of the bag.</td>
+                  </tr>
+                  <tr>
+                    <td style="padding-right: 20px; width: 40%"><b>Mesh fabric divider</b></td>
+                    <td>Provides better organization to save you time packing and unpacking.</td>
+                  </tr>
+                  <tr>
+                    <td style="padding-right: 20px; width: 40%"><b>Exterior pockets</b></td>
+                    <td>Convenient “on the go” access for your passport, phone and other small items.</td>
+                  </tr>
+                  <tr>
+                    <td style="padding-right: 20px; width: 40%"><b>Petzl <nobr>H-frame</nobr> Screw lock Carabiner</b></td>
+                    <td>Securely attaches straps so you can travel with confidence.</td>
+                  </tr>
+                  <tr>
+                    <td style="padding-right: 20px; width: 40%"><b>Dyneema® fabric</b></td>
+                    <td>Rugged and extremely light weight, 3x’s stronger than Kevlar and 15x’s stronger than steel.</td>
+                  </tr>
+                  <tr>
+                    <td style="padding-right: 20px; width: 40%"><b>Rolltop</b></td>
+                    <td>For when you need extra storage or have oversized items to carry.</td>
+                  </tr>
+                  <tr>
+                    <td style="padding-right: 20px; width: 40%"><b>Waterproof Zippers</b></td>
+                    <td>Slide smoothly to close and protect your belongings from the outside environment.</td>
+                  </tr>
+                  <tr>
+                    <td style="padding-right: 20px; width: 40%"><b>Laptop case</b></td>
+                    <td>For quick trips out to a coffee shop or meeting, leave your bag where you’re staying.</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <!-- <img src="@/assets/images/desktop/product/01.png" class="product--fw-img" /> -->
+          <video width="100%" controls preload="true" poster="@/assets/images/desktop/product/01.png">
+            <source src="@/assets/video/blackmile_backpack.mp4" type="video/mp4">
+            Sorry, your browser doesn't support embedded videos.
+          </video>
+
+          <img src="@/assets/images/desktop/product/02.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/03.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/04.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/05.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/06.png" class="product--fw-img" />
+
+          <!-- <img src="@/assets/images/desktop/product/07.png" class="product--fw-img" /> -->
+          <video width="100%" controls preload="true" poster="@/assets/images/desktop/product/07.png">
+            <source src="@/assets/video/blackmile_shoulder.mp4" type="video/mp4">
+            Sorry, your browser doesn't support embedded videos.
+          </video>
+
+          <img src="@/assets/images/desktop/product/08.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/09.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/10.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/11.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/12.png" class="product--fw-img" />
+
+          <!-- <img src="@/assets/images/desktop/product/13.png" class="product--fw-img" /> -->
+          <video width="100%" controls preload="true" poster="@/assets/images/desktop/product/13.png">
+            <source src="@/assets/video/blackmile_closing.mp4" type="video/mp4">
+            Sorry, your browser doesn't support embedded videos.
+          </video>
+
+          <img src="@/assets/images/desktop/product/14.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/15.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/16.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/17.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/18.png" class="product--fw-img" />
+
+          <!-- <img src="@/assets/images/desktop/product/19.png" class="product--fw-img" /> -->
+          <video width="100%" controls preload="true" poster="@/assets/images/desktop/product/19.png">
+            <source src="@/assets/video/blackmile_unpack.mp4" type="video/mp4">
+            Sorry, your browser doesn't support embedded videos.
+          </video>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+  $desktop: "(min-width: 768px)";
+  $mobile: "(max-width: 768px)";
+
+  .desktop {
+    @media #{$mobile} {
+      display: none;
+    }
+  }
+
+  .mobile {
+    @media #{$desktop} {
+      display: none;
+    }
+  }
+
+  .product {
+    background-color: white;
+    position: relative;
+
+    &--fw-img {
+      width: 100%;
+      margin-bottom: 10px;
+      display: block;
+
+      @media #{$desktop} {
+        margin-bottom: 20px;
+      }
+    }
+
+    &--btn {
+      background-color: var(--main-font-color);
+      border: none;
+      outline: none;
+      display: block;
+      width: 100%;
+      text-align: center;
+      font-size: 20px;
+      padding: 20px 0;
+      margin: 25px 0;
+      text-transform: uppercase;
+      cursor: pointer;
+
+      @media #{$desktop} {
+        padding: 15px 0;
+        font-size: 14px;
+      }
+    }
+
+    &--container {
+      padding: 0 25px;
+    }
+
+    &--header {
+      padding: 15px 20px;
+      background-color: #222222;
+      display: grid;
+      align-items: center;
+      grid-template-columns: 1fr auto;
+
+      svg {
+        width: 160px;
+      }
+
+      &--burger {
+        width: 30px;
+        height: 17px;
+        border-top: 1px solid white;
+        border-bottom: 1px solid white;
+        cursor: pointer;
+        position: relative;
+
+        &:after {
+          content: '';
+          position: absolute;
+          width: 100%;
+          border-bottom: 1px solid white;
+          top: 49%;
+        }
+      }
+    }
+
+    &--content {
+      padding: 40px 0;
+      max-width: 1024px;
+      margin: 0 auto;
+
+      @media #{$desktop} {
+        padding: 80px 0;
+      }
+
+      h4 {
+        font-family: 'Avenir Next', sans-serif;
+        font-size: 24px;
+        display: grid;
+        grid-template-columns: 1fr auto;
+        align-items: end;
+        margin: 0 0 20px;
+
+        span {
+          font-family: 'Avenir Next', sans-serif;
+          font-size: 16px;
+          font-weight: bold;
+        }
+      }
+
+      h5 {
+        font-family: 'Avenir Next', sans-serif;
+        font-weight: 600;
+        letter-spacing: 1.14px;
+        font-size: 23px;
+        margin-bottom: 10px;
+      }
+
+      p {
+        line-height: 1.7;
+      }
+
+      ul {
+        line-height: 1.7;
+        padding: 0 0 0 20px;
+        margin: 0;
+      }
+    }
+
+    &--grid {
+      display: grid;
+      grid-template-columns: 1.1fr .9fr;
+      grid-column-gap: 30px;
+      font-size: 14px;
+      min-width: 1000px;
+    }
+
+    table {
+      width: 100%;
+      border-collapse: collapse;
+      border-spacing: 10px;
+
+      thead {
+        border-bottom: 1px solid var(--main-font-color);
+      }
+
+      tbody {
+        td {
+          padding: 10px 0;
+          line-height: 1.6;
+        }
+      }
+    }
+  }
+</style>
