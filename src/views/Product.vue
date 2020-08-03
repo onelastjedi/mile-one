2cb09aa71b83034db2f97721e9c89e9ce9eb2ee2
Anton K <mi3ta@sent.as>
2020-08-03 21:16:57 +0300

    Fixed typos

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -122,145 +122,145 @@
       </div>
     </div>
 
-      <div class="product desktop">
-        <div class="product--content">
-          <div class="product--container">
-            <div class="product--grid">
-              <div class="product--column">
-                <img src="@/assets/images/mobile/product/01.png" class="product--fw-img" />
-                <img src="@/assets/images/mobile/product/02.png" class="product--fw-img" />
-                <img src="@/assets/images/mobile/product/03.png" class="product--fw-img" />
-                <img src="@/assets/images/mobile/product/04.png" class="product--fw-img" />
-              </div>
-
-              <div class="product--column">
-                <h4>The Mile One <span>$499.00</span></h4>
-                <p>The Mile One is designed to be the world’s best one bag travel bag. Through extensive research and development, choosing only the highest quality materials and components, it was refined over countless iterations to be your “go to” bag.</p>
-                <p>We admit not everyone has the same attention to detail and eye for perfection. The Mile One is made for discerning individuals who appreciate intelligent design and expert craftsmanship — without compromises.</p>
-                <button class="product--btn">Add to cart</button>
-
-                <h5>Materials</h5>
-                <ul>
-                  <li>Fabric: Dyneema®, the world’s strongest fiber™</li>
-                  <li>Hardware: Aluminum Carabiners and Closures</li>
-                </ul>
-
-                <h5>Rationale</h5>
-                <p>Typical travel bags are big, clunky, noisome and troublesome. They feel like “luggage” when you are “lugging” them around. Many bags have useful features, yet they all seem to be lacking one. Or, on the flip side, many have far too many pockets or closures to make them practical for everyday use.</p>
-
-                <h5>Development</h5>
-                <p>The Mile One is the first travel bag designed by Black Mile founder Charley with his friend and admitted gear geek, Dan. Based on our experiences traveling the globe and testing gear from dozens of manufacturers, we worked through countless prototypes to create the Mile One.</p>
-                <p>We kept everything we liked and got rid of everything we didn’t like — or need — in the process. The result is a minimalist design that maximizes versatility through a multi-functional approach.</p>
-
-                <table>
-                  <thead>
-                    <tr>
-                      <td><h5>Feature</h5></td>
-                      <td><h5>Benefit</h5></td>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td style="padding-right: 20px; width: 40%"><b>Backpack, shoulder pack or duffle</b></td>
-                      <td>Multi-function design allows you to configure the pack based on use and load.</td>
-                    </tr>
-                    <tr>
-                      <td style="padding-right: 20px; width: 40%"><b>Opens like a suitcase</b></td>
-                      <td>Easily access the contents without “digging” to get to the bottom of the bag.</td>
-                    </tr>
-                    <tr>
-                      <td style="padding-right: 20px; width: 40%"><b>Mesh fabric divider</b></td>
-                      <td>Provides better organization to save you time packing and unpacking.</td>
-                    </tr>
-                    <tr>
-                      <td style="padding-right: 20px; width: 40%"><b>Exterior pockets</b></td>
-                      <td>Convenient “on the go” access for your passport, phone and other small items.</td>
-                    </tr>
-                    <tr>
-                      <td style="padding-right: 20px; width: 40%"><b>Petzl <nobr>H-frame</nobr> Screw lock Carabiner</b></td>
-                      <td>Securely attaches straps so you can travel with confidence.</td>
-                    </tr>
-                    <tr>
-                      <td style="padding-right: 20px; width: 40%"><b>Dyneema® fabric</b></td>
-                      <td>Rugged and extremely light weight, 3x’s stronger than Kevlar and 15x’s stronger than steel.</td>
-                    </tr>
-                    <tr>
-                      <td style="padding-right: 20px; width: 40%"><b>Rolltop</b></td>
-                      <td>For when you need extra storage or have oversized items to carry.</td>
-                    </tr>
-                    <tr>
-                      <td style="padding-right: 20px; width: 40%"><b>Waterproof Zippers</b></td>
-                      <td>Slide smoothly to close and protect your belongings from the outside environment.</td>
-                    </tr>
-                    <tr>
-                      <td style="padding-right: 20px; width: 40%"><b>Laptop case</b></td>
-                      <td>For quick trips out to a coffee shop or meeting, leave your bag where you’re staying.</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
+    <div class="product desktop">
+      <div class="product--content">
+        <div class="product--container">
+          <div class="product--grid">
+            <div class="product--column">
+              <img src="@/assets/images/mobile/product/01.png" class="product--fw-img" />
+              <img src="@/assets/images/mobile/product/02.png" class="product--fw-img" />
+              <img src="@/assets/images/mobile/product/03.png" class="product--fw-img" />
+              <img src="@/assets/images/mobile/product/04.png" class="product--fw-img" />
             </div>
 
-            <player class="product--player"
-              headline="Quickly Converts to a Backpack"
-              poster="assets/images/desktop/product/01.png"
-              source="https://blackmile-video-storage.s3-us-west-2.amazonaws.com/blackmile_backpack.mp4" />
+            <div class="product--column">
+              <h4>The Mile One <span>$499.00</span></h4>
+              <p>The Mile One is designed to be the world’s best one bag travel bag. Through extensive research and development, choosing only the highest quality materials and components, it was refined over countless iterations to be your “go to” bag.</p>
+              <p>We admit not everyone has the same attention to detail and eye for perfection. The Mile One is made for discerning individuals who appreciate intelligent design and expert craftsmanship — without compromises.</p>
+              <button class="product--btn">Add to cart</button>
 
-            <!-- <video width="100%" controls preload="true" poster="@/assets/images/desktop/product/01.png">
-              <source src="https://blackmile-video-storage.s3-us-west-2.amazonaws.com/blackmile_backpack.mp4" type="video/mp4">
-              Sorry, your browser doesn't support embedded videos.
-            </video> -->
+              <h5>Materials</h5>
+              <ul>
+                <li>Fabric: Dyneema®, the world’s strongest fiber™</li>
+                <li>Hardware: Aluminum Carabiners and Closures</li>
+              </ul>
 
-            <img src="@/assets/images/desktop/product/02.png" class="product--fw-img" />
-            <img src="@/assets/images/desktop/product/03.png" class="product--fw-img" />
-            <img src="@/assets/images/desktop/product/04.png" class="product--fw-img" />
-            <img src="@/assets/images/desktop/product/05.png" class="product--fw-img" />
-            <img src="@/assets/images/desktop/product/06.png" class="product--fw-img" />
+              <h5>Rationale</h5>
+              <p>Typical travel bags are big, clunky, noisome and troublesome. They feel like “luggage” when you are “lugging” them around. Many bags have useful features, yet they all seem to be lacking one. Or, on the flip side, many have far too many pockets or closures to make them practical for everyday use.</p>
 
-            <player class="product--player"
-              headline="Quickly Converts to a Shoulder Bag"
-              poster="assets/images/desktop/product/07.png"
-              source="https://blackmile-video-storage.s3-us-west-2.amazonaws.com/blackmile_shoulder.mp4" />
+              <h5>Development</h5>
+              <p>The Mile One is the first travel bag designed by Black Mile founder Charley with his friend and admitted gear geek, Dan. Based on our experiences traveling the globe and testing gear from dozens of manufacturers, we worked through countless prototypes to create the Mile One.</p>
+              <p>We kept everything we liked and got rid of everything we didn’t like — or need — in the process. The result is a minimalist design that maximizes versatility through a multi-functional approach.</p>
 
-            <!-- <video width="100%" controls preload="true" poster="@/assets/images/desktop/product/07.png">
-              <source src="https://blackmile-video-storage.s3-us-west-2.amazonaws.com/blackmile_shoulder.mp4" type="video/mp4">
-              Sorry, your browser doesn't support embedded videos.
-            </video> -->
-
-            <img src="@/assets/images/desktop/product/08.png" class="product--fw-img" />
-            <img src="@/assets/images/desktop/product/09.png" class="product--fw-img" />
-            <img src="@/assets/images/desktop/product/10.png" class="product--fw-img" />
-            <img src="@/assets/images/desktop/product/11.png" class="product--fw-img" />
-            <img src="@/assets/images/desktop/product/12.png" class="product--fw-img" />
-
-            <player class="product--player"
-              headline="Headline About Closing the Bag"
-              poster="assets/images/desktop/product/13.png"
-              source="https://blackmile-video-storage.s3-us-west-2.amazonaws.com/blackmile_closing.mp4" />
-
-            <!-- <video width="100%" controls preload="true" poster="@/assets/images/desktop/product/13.png">
-              <source src="https://blackmile-video-storage.s3-us-west-2.amazonaws.com/blackmile_closing.mp4" type="video/mp4">
-              Sorry, your browser doesn't support embedded videos.
-            </video> -->
-
-            <img src="@/assets/images/desktop/product/14.png" class="product--fw-img" />
-            <img src="@/assets/images/desktop/product/15.png" class="product--fw-img" />
-            <img src="@/assets/images/desktop/product/16.png" class="product--fw-img" />
-            <img src="@/assets/images/desktop/product/17.png" class="product--fw-img" />
-            <img src="@/assets/images/desktop/product/18.png" class="product--fw-img" />
-
-            <player class="product--player"
-              headline="Headline About the Interior"
-              poster="assets/images/desktop/product/19.png"
-              source="https://blackmile-video-storage.s3-us-west-2.amazonaws.com/blackmile_unpack.mp4" />
-
-            <!-- <video width="100%" controls preload="true" poster="@/assets/images/desktop/product/19.png">
-              <source src="https://blackmile-video-storage.s3-us-west-2.amazonaws.com/blackmile_unpack.mp4" type="video/mp4">
-              Sorry, your browser doesn't support embedded videos.
-            </video> -->
+              <table>
+                <thead>
+                  <tr>
+                    <td><h5>Feature</h5></td>
+                    <td><h5>Benefit</h5></td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td style="padding-right: 20px; width: 40%"><b>Backpack, shoulder pack or duffle</b></td>
+                    <td>Multi-function design allows you to configure the pack based on use and load.</td>
+                  </tr>
+                  <tr>
+                    <td style="padding-right: 20px; width: 40%"><b>Opens like a suitcase</b></td>
+                    <td>Easily access the contents without “digging” to get to the bottom of the bag.</td>
+                  </tr>
+                  <tr>
+                    <td style="padding-right: 20px; width: 40%"><b>Mesh fabric divider</b></td>
+                    <td>Provides better organization to save you time packing and unpacking.</td>
+                  </tr>
+                  <tr>
+                    <td style="padding-right: 20px; width: 40%"><b>Exterior pockets</b></td>
+                    <td>Convenient “on the go” access for your passport, phone and other small items.</td>
+                  </tr>
+                  <tr>
+                    <td style="padding-right: 20px; width: 40%"><b>Petzl <nobr>H-frame</nobr> Screw lock Carabiner</b></td>
+                    <td>Securely attaches straps so you can travel with confidence.</td>
+                  </tr>
+                  <tr>
+                    <td style="padding-right: 20px; width: 40%"><b>Dyneema® fabric</b></td>
+                    <td>Rugged and extremely light weight, 3x’s stronger than Kevlar and 15x’s stronger than steel.</td>
+                  </tr>
+                  <tr>
+                    <td style="padding-right: 20px; width: 40%"><b>Rolltop</b></td>
+                    <td>For when you need extra storage or have oversized items to carry.</td>
+                  </tr>
+                  <tr>
+                    <td style="padding-right: 20px; width: 40%"><b>Waterproof Zippers</b></td>
+                    <td>Slide smoothly to close and protect your belongings from the outside environment.</td>
+                  </tr>
+                  <tr>
+                    <td style="padding-right: 20px; width: 40%"><b>Laptop case</b></td>
+                    <td>For quick trips out to a coffee shop or meeting, leave your bag where you’re staying.</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
+
+          <player class="product--player"
+            headline="Quickly Converts to a Backpack"
+            poster="assets/images/desktop/product/01.png"
+            source="https://blackmile-video-storage.s3-us-west-2.amazonaws.com/blackmile_backpack.mp4" />
+
+          <!-- <video width="100%" controls preload="true" poster="@/assets/images/desktop/product/01.png">
+            <source src="https://blackmile-video-storage.s3-us-west-2.amazonaws.com/blackmile_backpack.mp4" type="video/mp4">
+            Sorry, your browser doesn't support embedded videos.
+          </video> -->
+
+          <img src="@/assets/images/desktop/product/02.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/03.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/04.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/05.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/06.png" class="product--fw-img" />
+
+          <player class="product--player"
+            headline="Quickly Converts to a Shoulder Bag"
+            poster="assets/images/desktop/product/07.png"
+            source="https://blackmile-video-storage.s3-us-west-2.amazonaws.com/blackmile_shoulder.mp4" />
+
+          <!-- <video width="100%" controls preload="true" poster="@/assets/images/desktop/product/07.png">
+            <source src="https://blackmile-video-storage.s3-us-west-2.amazonaws.com/blackmile_shoulder.mp4" type="video/mp4">
+            Sorry, your browser doesn't support embedded videos.
+          </video> -->
+
+          <img src="@/assets/images/desktop/product/08.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/09.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/10.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/11.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/12.png" class="product--fw-img" />
+
+          <player class="product--player"
+            headline="Headline About Closing the Bag"
+            poster="assets/images/desktop/product/13.png"
+            source="https://blackmile-video-storage.s3-us-west-2.amazonaws.com/blackmile_closing.mp4" />
+
+          <!-- <video width="100%" controls preload="true" poster="@/assets/images/desktop/product/13.png">
+            <source src="https://blackmile-video-storage.s3-us-west-2.amazonaws.com/blackmile_closing.mp4" type="video/mp4">
+            Sorry, your browser doesn't support embedded videos.
+          </video> -->
+
+          <img src="@/assets/images/desktop/product/14.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/15.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/16.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/17.png" class="product--fw-img" />
+          <img src="@/assets/images/desktop/product/18.png" class="product--fw-img" />
+
+          <player class="product--player"
+            headline="Headline About the Interior"
+            poster="assets/images/desktop/product/19.png"
+            source="https://blackmile-video-storage.s3-us-west-2.amazonaws.com/blackmile_unpack.mp4" />
+
+          <!-- <video width="100%" controls preload="true" poster="@/assets/images/desktop/product/19.png">
+            <source src="https://blackmile-video-storage.s3-us-west-2.amazonaws.com/blackmile_unpack.mp4" type="video/mp4">
+            Sorry, your browser doesn't support embedded videos.
+          </video> -->
         </div>
       </div>
+    </div>
   </section>
 </template>
 
